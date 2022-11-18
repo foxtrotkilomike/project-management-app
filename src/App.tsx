46ed3685ad014b1ccd62799650e212983db05c83
Philipp Khromov <filooloog@mail.ru>
2022-11-18 09:05:58 +0300

    refactor: Button, add className to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import classes from './App.module.scss';
 import Header from './layouts/Header';
 
 const App = () => {
   return (
-    <div>
+    <div className={classes.root}>
       <Header />
     </div>
   );
--- src/commons/Button/index.tsx
+++ src/commons/Button/index.tsx
@@ -3,23 +3,21 @@ import classes from './Button.module.scss';
 import React, { MouseEvent } from 'react';
 import { ButtonTypes } from '../../config/types';
 
-const Button = ({ type, onClick, children }: Props): JSX.Element => {
-  const classNameMap: Record<typeof type, string> = {
-    filled: `${classes.btn} ${classes.filled}`,
-    contour: `${classes.btn} ${classes.contour}`,
-  };
-
-  return (
-    <button className={classNameMap[type]} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
+const Button = ({ type, onClick, children }: Props): JSX.Element => (
+  <button className={classNameMap[type]} onClick={onClick}>
+    {children}
+  </button>
+);
 
 type Props = {
   type: ButtonTypes;
   onClick: (e: MouseEvent) => void;
   children: React.ReactNode;
+};
+
+const classNameMap: Record<Props['type'], string> = {
+  filled: `${classes.btn} ${classes.filled}`,
+  contour: `${classes.btn} ${classes.contour}`,
 };
 
 export default Button;
