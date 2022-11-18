53f068ab7425053496e32377f87688047ed28b9a
Philipp Khromov <filooloog@mail.ru>
2022-11-18 15:22:06 +0300

    feat: add CenteredContainer to Header, fix styles

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -3,6 +3,7 @@ import { buttonsText, selectData } from '../../config/data';
 import Button from '../../commons/Button';
 import Select from '../../commons/Select';
 import Logo from '../../commons/Logo';
+import CenteredContainer from '../../commons/CenteredContainer';
 
 const Header = ({ sticky }: Props): JSX.Element => {
   const headerClassName = sticky ? `${classes.header} ${classes.headerSticky}` : classes.header;
@@ -10,16 +11,20 @@ const Header = ({ sticky }: Props): JSX.Element => {
 
   return (
     <header className={headerClassName}>
-      <Logo />
-      <div className={classes.buttonsContainer}>
-        <Button type="filled" onClick={() => {}}>
-          {buttonsText.signUp}
-        </Button>
-        <Button type="contour" onClick={() => {}}>
-          {buttonsText.signIn}
-        </Button>
-        <Select {...languageSelectData} />
-      </div>
+      <CenteredContainer>
+        <div className={classes.headerContent}>
+          <Logo />
+          <div className={classes.buttonsContainer}>
+            <Button type="filled" onClick={() => {}}>
+              {buttonsText.signUp}
+            </Button>
+            <Button type="contour" onClick={() => {}}>
+              {buttonsText.signIn}
+            </Button>
+            <Select {...languageSelectData} />
+          </div>
+        </div>
+      </CenteredContainer>
     </header>
   );
 };
