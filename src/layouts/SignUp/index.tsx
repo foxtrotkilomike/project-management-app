a71def83b1f4272e5d536a7b50eb6afbc8ec33b4
Philipp Khromov <filooloog@mail.ru>
2022-11-23 13:55:06 +0300

    feat: add SignUp page template

---
+++ src/layouts/SignUp/index.tsx
@@ -0,0 +1,17 @@
+import classes from './SignUp.module.scss';
+
+const SignUp = ({}: Props): JSX.Element => {
+  return (
+    <div>
+      This is Sign Up page
+      {/*
+        Here will be the LoginForm component of variant 'signUp'
+        <LoginForm variant="signUp">
+      */}
+    </div>
+  );
+};
+
+type Props = Record<string, string>;
+
+export default SignUp;
