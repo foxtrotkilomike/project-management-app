37fd341c0b4046f34ee1fb29fa499075214c5483
Philipp Khromov <filooloog@mail.ru>
2022-11-23 13:57:06 +0300

    feat: add RouterProvider, Outlet to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './styles/index.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from './config/routes';
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
