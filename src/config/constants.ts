3d4bf9878048ef4eeae9d690f5f84d00e5bfe786
Philipp Khromov <filooloog@mail.ru>
2022-11-24 13:05:26 +0300

    refactor: PR fixes

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+enum ResponseStatus {
+  'NOT_AUTHORIZED' = 401,
+  'UNHANDLED_REJECTION' = 503,
+}
+
+export { ResponseStatus };
--- src/layouts/ErrorBoundary/index.tsx
+++ src/layouts/ErrorBoundary/index.tsx
@@ -1,16 +1,17 @@
 import classes from './ErrorBoundary.module.scss';
 import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+import { ResponseStatus } from '../../config/constants';
 
 const ErrorBoundary = ({}: Props): JSX.Element => {
   const error = useRouteError() as Error;
   console.error(error);
 
   if (isRouteErrorResponse(error)) {
-    if (error.status === 401) {
+    if (error.status === ResponseStatus.NOT_AUTHORIZED) {
       return <p className={classes.root}>You are not authorized to view this page</p>;
     }
 
-    if (error.status === 503) {
+    if (error.status === ResponseStatus.UNHANDLED_REJECTION) {
       return (
         <div className={classes.root}>
           <p className={classes.errorTitle}>Sorry, an unexpected error has occurred</p>
