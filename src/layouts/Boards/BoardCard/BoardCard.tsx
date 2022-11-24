0bffd0b0e2a8d99d88ba41e0f16d8dd54352d763
Alina <malinka775@yandex.ru>
2022-11-24 19:43:10 +0200

    refactor: rewrite according to comments

--- src/layouts/Boards/BoardCard/BoardCard.tsx
+++ src/layouts/Boards/BoardCard/BoardCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import classes from './BoardCard.module.scss';
@@ -6,29 +6,41 @@ import BoardInfoItem from './BoardInfoItem';
 import { BoardInfoItemProps } from './BoardInfoItem/BoardInfoItem';
 import TrashcanIcon from '../../../assets/svg/trash.svg';
 import classNames from 'classnames';
+import { isLargeMobile } from '../../../helpers/getWindowWidth';
 
 export const BoardCard = (props: BoardProps): JSX.Element => {
-  const { onRemove, title, infoObject } = props;
+  const [isMobile, setIsMobile] = useState(isLargeMobile());
+  const { onRemove, title, metaData } = props;
 
-  const description = infoObject.map((item) => (
-    <BoardInfoItem key={item.propertyName} propertyName={item.propertyName} value={item.value} />
+  useEffect(() => {
+    window.addEventListener('resize', () => setIsMobile(isLargeMobile()));
+    return () => window.removeEventListener('resize', () => setIsMobile(isLargeMobile()));
+  });
+
+  const description = metaData.map((item) => (
+    <BoardInfoItem key={item.name} name={item.name} value={item.value} />
   ));
 
-  const iconClassnames = classNames(classes.button, classes.deleteIcon);
+  const iconClassNames = classNames(classes.button, classes.deleteIcon);
 
   return (
     <Card className={classes.boardCard}>
       <Card.Body className={classes.boardCard__content}>
         <div className={classes.boardCard__header}>
           <Card.Title className={classes.boardCard__title}>{title}</Card.Title>
-          <Button className={classes.button} variant="outline-danger" onClick={onRemove}>
-            Remove
-          </Button>
-          <Button className={iconClassnames} variant="outline-danger" onClick={onRemove}>
-            <img src={TrashcanIcon} alt="Remove" />
-          </Button>
+          {!isMobile ? (
+            <Button className={classes.button} variant="outline-danger" onClick={onRemove}>
+              Remove
+            </Button>
+          ) : (
+            <Button className={iconClassNames} variant="outline-danger" onClick={onRemove}>
+              <img src={TrashcanIcon} alt="Remove" />
+            </Button>
+          )}
         </div>
-        <Card.Text className={classes.boardCard__info}>{description}</Card.Text>
+        <Card.Text as="div" className={classes.boardCard__info}>
+          {description}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
@@ -36,6 +48,6 @@ export const BoardCard = (props: BoardProps): JSX.Element => {
 
 export type BoardProps = {
   title: string;
-  infoObject: BoardInfoItemProps[];
+  metaData: BoardInfoItemProps[];
   onRemove: (e: React.SyntheticEvent) => void;
 };
--- src/layouts/Boards/BoardCard/BoardInfoItem/BoardInfoItem.tsx
+++ src/layouts/Boards/BoardCard/BoardInfoItem/BoardInfoItem.tsx
@@ -1,16 +1,16 @@
 import classes from './BoardInfoItem.module.scss';
 
 export const BoardInfoItem = (props: BoardInfoItemProps): JSX.Element => {
-  const { propertyName, value } = props;
+  const { name, value } = props;
   return (
     <div className={classes.infoItem}>
-      <span className={classes.infoItem__text}>{propertyName}</span>
+      <span className={classes.infoItem__text}>{name}</span>
       <span className={classes.infoItem__quantity}>{value}</span>
     </div>
   );
 };
 
 export type BoardInfoItemProps = {
-  propertyName: string;
+  name: string;
   value: number;
 };
