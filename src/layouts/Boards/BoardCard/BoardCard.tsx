8fafb44a9764ee100a5a0bb3bcad57678cce2712
Alina <malinka775@yandex.ru>
2022-11-24 15:48:21 +0200

    feat: add media queries

--- src/layouts/Boards/BoardCard/BoardCard.tsx
+++ src/layouts/Boards/BoardCard/BoardCard.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
 import classes from './BoardCard.module.scss';
 import BoardInfoItem from './BoardInfoItem';
 import { BoardInfoItemProps } from './BoardInfoItem/BoardInfoItem';
+import TrashcanIcon from '../../../assets/svg/trash.svg';
+import classNames from 'classnames';
+
 export const BoardCard = (props: BoardProps): JSX.Element => {
   const { onRemove, title, infoObject } = props;
 
   const description = infoObject.map((item) => (
     <BoardInfoItem key={item.propertyName} propertyName={item.propertyName} value={item.value} />
   ));
+
+  const iconClassnames = classNames(classes.button, classes.deleteIcon);
 
   return (
     <Card className={classes.boardCard}>
@@ -19,6 +23,9 @@ export const BoardCard = (props: BoardProps): JSX.Element => {
           <Card.Title className={classes.boardCard__title}>{title}</Card.Title>
           <Button className={classes.button} variant="outline-danger" onClick={onRemove}>
             Remove
+          </Button>
+          <Button className={iconClassnames} variant="outline-danger" onClick={onRemove}>
+            <img src={TrashcanIcon} alt="Remove" />
           </Button>
         </div>
         <Card.Text className={classes.boardCard__info}>{description}</Card.Text>
