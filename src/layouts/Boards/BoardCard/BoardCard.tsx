197a9ed2bd19cd41b1085c25829cfda919d6e5c2
Alina <malinka775@yandex.ru>
2022-11-24 14:17:14 +0200

    feat: add props to BoardCard

--- src/layouts/Boards/BoardCard/BoardCard.tsx
+++ src/layouts/Boards/BoardCard/BoardCard.tsx
@@ -1,24 +1,27 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import classes from './BoardCard.module.scss';
-
+import BoardInfoItem from './BoardInfoItem';
+import { BoardInfoItemProps } from './BoardInfoItem/BoardInfoItem';
 export const BoardCard = (props: BoardProps): JSX.Element => {
-  const title = props.title;
+  const { onRemove, title, infoObject } = props;
+
+  const description = infoObject.map((item) => (
+    <BoardInfoItem key={item.propertyName} propertyName={item.propertyName} value={item.value} />
+  ));
 
   return (
     <Card className={classes.boardCard}>
       <Card.Body className={classes.boardCard__content}>
         <div className={classes.boardCard__header}>
           <Card.Title className={classes.boardCard__title}>{title}</Card.Title>
-          <Button variant="outline-danger" onClick={() => {}}>
-            Delete
+          <Button className={classes.button} variant="outline-danger" onClick={onRemove}>
+            Remove
           </Button>
         </div>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of the card's
-          content.
-        </Card.Text>
+        <Card.Text className={classes.boardCard__info}>{description}</Card.Text>
       </Card.Body>
     </Card>
   );
@@ -26,4 +29,6 @@ export const BoardCard = (props: BoardProps): JSX.Element => {
 
 export type BoardProps = {
   title: string;
+  infoObject: BoardInfoItemProps[];
+  onRemove: (e: React.SyntheticEvent) => void;
 };
