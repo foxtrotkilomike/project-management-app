946ad27e38dfb04f7393497693107cf60586ac71
Alina <malinka775@yandex.ru>
2022-11-25 19:26:42 +0200

    feat: add modal opening onAddClick

--- src/layouts/Board/BoardColumn/BoardColumn.tsx
+++ src/layouts/Board/BoardColumn/BoardColumn.tsx
@@ -1,25 +1,46 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Modal from '../../../commons/Modal';
+import TaskIcon from '../../../commons/Modal/TaskIcon';
+import { ITask, Task } from '../Task/Task';
 import classes from './BoardColumn.module.scss';
 
 export const BoardColumn = (props: BoardColumnProps): JSX.Element => {
+  const [isModalActive, setIsModalActive] = useState(false);
   const { title, tasks } = props;
 
-  const taskCards = tasks.map((item) => <div key={item}> {item}</div>);
+  const taskCards = tasks.map((item) => <Task key={item.id} {...item} />);
+
+  const openModal = () => {
+    setIsModalActive(true);
+  };
+
+  const closeModal = () => {
+    setIsModalActive(false);
+  };
+
+  // TODO: ADD FORM FOR ADDING A NEW TASK
 
   return (
-    <div className={classes.column}>
-      <div className={classes.header}>{title}</div>
-      <div className={classes.tasksWrapper}>{taskCards}</div>
-    </div>
+    <li className={classes.column}>
+      <div className={classes.header}>
+        <h4>{title}</h4>
+        <div className={classes.badge}>{tasks.length}</div>
+      </div>
+      <ul className={classes.tasksWrapper}>{taskCards}</ul>
+      <div className={classes.footer}>
+        <Button className={classes.add} variant="primary" onClick={openModal}>
+          + Add task
+        </Button>
+      </div>
+      <Modal title="Add new task" icon={<TaskIcon />} onHide={closeModal} isActive={isModalActive}>
+        Task adding form
+      </Modal>
+    </li>
   );
 };
 
 export type BoardColumnProps = {
   title: string;
-  tasks: string[];
+  tasks: ITask[];
 };
-
-export interface ITask {
-  id: string;
-  title: string;
-  description: string;
-}
