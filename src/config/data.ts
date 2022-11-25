3c15430daf9e90d031f97d2e3ecf50c2513dd9be
Philipp Khromov <filooloog@mail.ru>
2022-11-25 14:12:27 +0300

    feat: add login form data & types

--- src/config/data.ts
+++ src/config/data.ts
@@ -1,20 +1,11 @@
 import { SelectProps } from '../commons/Select';
 import GlobeIcon from '../assets/svg/globe2.svg';
+import { LoginForm, LoginPrompt } from './types';
 
 const buttonsText = {
   signUp: 'sign up',
   signIn: 'sign in',
 };
-
-const selectData: SelectProps[] = [
-  {
-    labelType: 'icon',
-    ariaLabel: 'Language',
-    options: ['EN', 'RU'],
-    activeOptionIndex: 0,
-    icon: GlobeIcon,
-  },
-];
 
 const footerData = {
   year: '2022',
@@ -34,4 +25,58 @@ const footerData = {
   ],
 };
 
-export { buttonsText, selectData, footerData };
+const loginFormData: LoginForm = {
+  signUp: {
+    userName: 'Name',
+    login: 'Login',
+    password: 'Password',
+    passwordRepeat: 'Repeat password',
+    submitButton: 'Sign up',
+    errors: {
+      userExists: 'User with such login already exists',
+      passwordMismatch: "Passwords don't not match",
+    },
+  },
+  signIn: {
+    login: 'Login',
+    password: 'Password',
+    submitButton: 'Log in',
+    errors: {
+      userDoesNotExists: 'User with such login does not exist',
+      incorrectPassword: 'Password is incorrect',
+    },
+  },
+  validationErrors: {
+    userNameRequired: 'User name is a required field',
+    passwordRequired: 'Password is a required field',
+    passwordLength: 'Password must be at least 8 characters long',
+  },
+};
+
+const loginHeading = {
+  signUp: 'Sign Up',
+  signIn: 'Sign In',
+};
+
+const loginPromptData: LoginPrompt = {
+  signUp: {
+    prompt: 'Already have an account?',
+    linkText: 'Sign in',
+  },
+  signIn: {
+    prompt: "Don't have an account?",
+    linkText: 'Sign up',
+  },
+};
+
+const selectData: SelectProps[] = [
+  {
+    labelType: 'icon',
+    ariaLabel: 'Language',
+    options: ['EN', 'RU'],
+    activeOptionIndex: 0,
+    icon: GlobeIcon,
+  },
+];
+
+export { buttonsText, footerData, loginFormData, loginHeading, loginPromptData, selectData };
