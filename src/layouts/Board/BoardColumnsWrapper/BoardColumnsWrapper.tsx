e634cab6daf691d248c2d8432354b9955ac256cd
Alina <malinka775@yandex.ru>
2022-11-27 18:57:05 +0200

    feat: add plus btn

--- src/layouts/Board/BoardColumnsWrapper/BoardColumnsWrapper.tsx
+++ src/layouts/Board/BoardColumnsWrapper/BoardColumnsWrapper.tsx
@@ -1,9 +1,33 @@
 import classes from './BoardColumnsWrapper.module.scss';
+import plusButton from '../../../assets/svg/plus.svg';
+import Modal from '../../../commons/Modal';
+import { useState } from 'react';
 
 export const BoardColumnsWrapper = (props: wrapperProps) => {
   const children = props.children;
-
-  return <ul className={classes.columnsWrapper}>{children}</ul>;
+  const [isModalActive, setIsModalActive] = useState(false);
+  const addColumn = () => {
+    //TODO implement add column
+    setIsModalActive(true);
+  };
+  const onHide = () => {
+    //TODO implement close modal func
+    setIsModalActive(false);
+  };
+  return (
+    <ul className={classes.columnsWrapper}>
+      {children}
+      <li>
+        <button className={classes.add} onClick={addColumn}>
+          <img src={plusButton} alt="add column" />
+        </button>
+      </li>
+      <Modal isActive={isModalActive} onHide={onHide} title="New column">
+        {/* TODO: add form */}
+        Column adding form
+      </Modal>
+    </ul>
+  );
 };
 
 export type wrapperProps = {
