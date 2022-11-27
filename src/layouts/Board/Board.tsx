e5a63cbb7ede2693519490ab5b92ef97c23de0a5
Alina <malinka775@yandex.ru>
2022-11-27 21:56:48 +0200

    refactor: change according to linter requirements

--- src/layouts/Board/Board.tsx
+++ src/layouts/Board/Board.tsx
@@ -5,10 +5,9 @@ import classes from './Board.module.scss';
 import BoardColumn from './BoardColumn';
 import BoardColumnsWrapper from './BoardColumnsWrapper';
 
-
 // TODO: get rid of MOCKED CONSTANTS
 
-const MOCK_BOARD = {title: 'Title', _id: 'board_id', owner: 'owner', users:['user1', 'user2']};
+const MOCK_BOARD = { title: 'Title', _id: 'board_id', owner: 'owner', users: ['user1', 'user2'] };
 const MOCK_COLUMNS = [
   {
     _id: 'column 1',
@@ -27,8 +26,8 @@ const MOCK_COLUMNS = [
     title: 'column 3',
     order: 3,
     boardId: 'board_id',
-  }
-]
+  },
+];
 const MOCK_TASKS = {
   'column 1': [
     {
@@ -50,7 +49,7 @@ const MOCK_TASKS = {
       description: 'decription of task',
       userId: 'user id',
       users: ['Alina', 'Phil', 'Stas', 'Nastya'],
-    }
+    },
   ],
   'column 2': [
     {
@@ -74,21 +73,24 @@ const MOCK_TASKS = {
       description: 'decription of task',
       userId: 'user id',
       users: ['Alina', 'Phil', 'Stas', 'Nastya'],
-    }
+    },
   ],
 };
 
 // TODO: get rid of MOCKED FUNCTIONS
-const getBoard = (id: string) => Promise.resolve({json: () => (MOCK_BOARD)}).then((res) => res.json()); //TODO: implement getting board by id;
-const getColumns = (id: string) =>  Promise.resolve({json: () => (MOCK_COLUMNS)}).then((res) => res.json());  //TODO: implement getting columns by board id;
-const getTasks = (id: string) => Promise.resolve({json: () => (MOCK_TASKS[id])}).then((res) => res.json()); //TODO: implement getting tasks by board id;
+const getBoard = (id: string) =>
+  Promise.resolve({ json: () => MOCK_BOARD }).then((res) => res.json()); //TODO: implement getting board by id;
+const getColumns = (id: string) =>
+  Promise.resolve({ json: () => MOCK_COLUMNS }).then((res) => res.json()); //TODO: implement getting columns by board id;
+const getTasks = (id: string) =>
+  Promise.resolve({ json: () => MOCK_TASKS[id] }).then((res) => res.json()); //TODO: implement getting tasks by board id;
 
 const fillColumnWithTasks = async (column: ColumnResponseType): Promise<ColumnModel> => {
   const tasks = await getTasks(column._id);
   return {
     ...column,
     tasks,
-  }
+  };
 };
 
 export const Board = (): JSX.Element => {
@@ -99,56 +101,54 @@ export const Board = (): JSX.Element => {
   useEffect(() => {
     getBoard(boardId).then((board) => setBoard(board));
     getColumns(boardId).then(async (columns) => {
-      const columnModels = await Promise.all(columns.map(async (column) => fillColumnWithTasks(column)));
+      const columnModels = await Promise.all(
+        columns.map(async (column) => fillColumnWithTasks(column))
+      );
       setColumns(columnModels);
     });
   }, []);
 
-  
-  const renderColumns = columns.map((column) => <BoardColumn key={column._id} {...column}/>)
+  const renderColumns = columns.map((column) => <BoardColumn key={column._id} {...column} />);
 
   return (
     <Container centered>
       <div className={classes.board}>
         <h1 className={classes.board__title}>{board?.title}</h1>
-        <BoardColumnsWrapper>
-          {renderColumns}
-        </BoardColumnsWrapper>
+        <BoardColumnsWrapper>{renderColumns}</BoardColumnsWrapper>
       </div>
     </Container>
   );
 };
 
 export type BoardType = {
-  _id: string,
-  title: string,
-  owner: string,
-  users: string[],
-} 
+  _id: string;
+  title: string;
+  owner: string;
+  users: string[];
+};
 
 export type ColumnResponseType = {
-  _id: string,
-  title: string,
-  order: number,
-  boardId: string,
-}
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+};
 
 export type ColumnModel = {
-  _id: string,
-  title: string,
-  order: number,
-  boardId: string,
-  tasks: TaskType[],
-}
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+  tasks: TaskType[];
+};
 
 export type TaskType = {
-  _id: string,
-  title: string,
-  order: number,
-  boardId: string,
-  columnId: string,
-  description: string,
-  userId: string,
-  users: string[],
-}
-
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+  columnId: string;
+  description: string;
+  userId: string;
+  users: string[];
+};
