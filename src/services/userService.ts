9c2a4838a36b2ce2ed654ea017fd6c8305c93424
Philipp Khromov <filooloog@mail.ru>
2022-11-28 20:34:39 +0300

    feat: add getUserById

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,5 +3,7 @@ import axios, { Endpoints } from './api';
 const usersEndpoint = Endpoints.users;
 
 const getAllUsers = async () => axios.get(usersEndpoint).then(({ data }) => data);
+const getUserById = async (id: string) =>
+  axios.get(`${usersEndpoint}/${id}`).then(({ data }) => data);
 
-export { getAllUsers };
+export { getAllUsers, getUserById };
