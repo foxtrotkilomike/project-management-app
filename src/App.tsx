c91cd65e827fbe4e7ce188d2a7f48f29d7075737
Philipp Khromov <filooloog@mail.ru>
2022-12-01 22:13:48 +0300

    feat: add AppContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import classes from './App.module.scss';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 import { Outlet } from 'react-router-dom';
+import AppContextProvider from './contexts/AppContextProvider';
 
 const App = () => {
   return (
-    <div className={classes.root}>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <AppContextProvider>
+      <div className={classes.root}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </AppContextProvider>
   );
 };
 
