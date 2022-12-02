f48652c6cd1c5920c372829529c507631a97e908
Philipp Khromov <filooloog@mail.ru>
2022-12-02 21:29:02 +0300

    refactor: imlement TwoColumnsFlex on LogIn page

--- src/layouts/LogIn/LogIn.tsx
+++ src/layouts/LogIn/LogIn.tsx
@@ -1,38 +1,34 @@
-import classes from './LogIn.module.scss';
 import { LoginFormType } from '../../config/types';
 
 import NavigationButton from '../../commons/NavigationButton';
-import Container from '../../commons/Container';
 import LoginForm from '../../commons/LoginForm';
 import LoginPrompt from '../../commons/LoginPrompt';
 
 import ArrowLeftIcon from '../../assets/svg/arrow-left.svg';
 import { routes } from '../../config/routes';
 import { loginHeading } from '../../config/data';
+import TwoColumnsFlex from '../TwoColumnsFlex';
 
 export const LogIn = ({ type }: LogInProps): JSX.Element => {
-  return (
-    <Container centered main growing>
-      <div className={classes.logIn__wrapper}>
-        <aside>
-          <NavigationButton
-            location={routes.MAIN}
-            size="lg"
-            icon={ArrowLeftIcon}
-            text="Back"
-            ariaLabel="Navigate back"
-          />
-        </aside>
-        <main className={classes.mainContent}>
-          <Container centered>
-            <h1>{loginHeading[type]}</h1>
-            <LoginForm type={type} />
-            <LoginPrompt type={type} />
-          </Container>
-        </main>
-      </div>
-    </Container>
+  const leftColumn = (
+    <NavigationButton
+      location={routes.MAIN}
+      size="lg"
+      icon={ArrowLeftIcon}
+      text="Back"
+      ariaLabel="Navigate back"
+    />
   );
+
+  const rightColumn = (
+    <>
+      <h1>{loginHeading[type]}</h1>
+      <LoginForm type={type} />
+      <LoginPrompt type={type} />
+    </>
+  );
+
+  return <TwoColumnsFlex leftColumn={leftColumn} rightColumn={rightColumn} />;
 };
 
 type LogInProps = {
