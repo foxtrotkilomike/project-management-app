670d192c3a3bbb641fc4a3e4036008f95c7c931f
Alina <malinka775@yandex.ru>
2022-12-03 23:44:56 +0200

    feat: add possibility to add an empty card (remaining size)

--- src/layouts/Boards/BoardCard/BoardCard.tsx
+++ src/layouts/Boards/BoardCard/BoardCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import classes from './BoardCard.module.scss';
@@ -10,44 +10,60 @@ import { isLargeMobile } from '../../../helpers/getWindowWidth';
 
 export const BoardCard = (props: BoardProps): JSX.Element => {
   const [isMobile, setIsMobile] = useState(isLargeMobile());
-  const { onRemove, title, metaData } = props;
-
   useEffect(() => {
     window.addEventListener('resize', () => setIsMobile(isLargeMobile()));
     return () => window.removeEventListener('resize', () => setIsMobile(isLargeMobile()));
   });
+  if ('children' in props) {
+    const { children, className } = props;
+    const cardClassNames = classNames(classes.boardCard, className);
+    return (
+      <Card className={cardClassNames}>
+        <Card.Body className={classes.boardCard__content}>
+          <Card.Text as="div" className={classes.boardCard__info}>
+            {children}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  } else {
+    const { onRemove, title, metaData, className } = props;
+    const cardClassNames = classNames(classes.boardCard, className);
+    const renderDescription = metaData.map((item) => (
+      <BoardInfoItem key={item.name} name={item.name} value={item.value} />
+    ));
 
-  const renderDescription = metaData.map((item) => (
-    <BoardInfoItem key={item.name} name={item.name} value={item.value} />
-  ));
+    const iconClassNames = classNames(classes.button, classes.deleteIcon);
 
-  const iconClassNames = classNames(classes.button, classes.deleteIcon);
-
-  return (
-    <Card className={classes.boardCard}>
-      <Card.Body className={classes.boardCard__content}>
-        <div className={classes.boardCard__header}>
-          <Card.Title className={classes.boardCard__title}>{title}</Card.Title>
-          {!isMobile ? (
-            <Button className={classes.button} variant="outline-danger" onClick={onRemove}>
-              Remove
-            </Button>
-          ) : (
-            <Button className={iconClassNames} variant="outline-danger" onClick={onRemove}>
-              <img src={TrashcanIcon} alt="trashcan" />
-            </Button>
-          )}
-        </div>
-        <Card.Text as="div" className={classes.boardCard__info}>
-          {renderDescription}
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+    return (
+      <Card className={cardClassNames}>
+        <Card.Body className={classes.boardCard__content}>
+          <div className={classes.boardCard__header}>
+            <Card.Title className={classes.boardCard__title}>{title}</Card.Title>
+            {!isMobile ? (
+              <Button className={classes.button} variant="outline-danger" onClick={(e) => onRemove}>
+                Remove
+              </Button>
+            ) : (
+              <Button className={iconClassNames} variant="outline-danger" onClick={(e) => onRemove}>
+                <img src={TrashcanIcon} alt="trashcan" />
+              </Button>
+            )}
+          </div>
+          <Card.Text as="div" className={classes.boardCard__info}>
+            {renderDescription}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
 };
 
-export type BoardProps = {
-  title: string;
-  metaData: BoardInfoItemProps[];
-  onRemove: (e: React.SyntheticEvent) => void;
-};
+export type BoardProps =
+  | {
+      title: string;
+      metaData: BoardInfoItemProps[];
+      onRemove: (id: string) => void;
+      className: string;
+    }
+  | { children: ReactNode; className: string };
