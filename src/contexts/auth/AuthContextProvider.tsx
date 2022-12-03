c74d90a3ec7ccc28b3e66aaffea51299499f4849
Philipp Khromov <filooloog@mail.ru>
2022-12-03 13:37:04 +0300

    refactor: replace type User for UserResponse

--- src/contexts/auth/AuthContextProvider.tsx
+++ src/contexts/auth/AuthContextProvider.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { AppData, User } from '../../config/types';
+import { AppData } from '../../config/types';
+import { UserResponse } from '../../services/users/types';
 import { getAppData } from '../../services/authService';
 import { AuthContext } from './authContext';
 
@@ -8,14 +9,14 @@ const getUserData = (key: keyof AppData) => {
   return userData ? userData : '';
 };
 
-const initializeUserData = (): User => ({
+const initializeUserData = (): UserResponse => ({
   _id: getUserData('userId'),
   name: getUserData('userName'),
   login: getUserData('userLogin'),
 });
 
 const AuthContextProvider = ({ children }: AuthContextProviderProps): JSX.Element => {
-  const [user, setUser] = useState<User>(initializeUserData);
+  const [user, setUser] = useState<UserResponse>(initializeUserData);
 
   return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
 };
